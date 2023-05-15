8f4aa9e1ceb99bba77bcbda05135070279b96e7f
Tasdiq823 <124677748+Tasdiq823@users.noreply.github.com>
2023-05-15 21:08:02 +0700

    Add files via upload

--- cv.php
+++ cv.php
@@ -1,16 +1,71 @@
-<?php
+<?= $this->extend('layout/cv_template'); ?>
+<?= $this->section('konten'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="row"><img src="/gkomik/me.jpg" width="200 px"></th>
 
-namespace App\Controllers;
-
-class Cv extends BaseController
-{
-    public function index()
-
-
-    {
-        $data = [
-            'alamat' => 'CV Pribadi'
-        ];
-        return view('tugas/cv', $data);
-    }
-}
+                    </tr>
+                    <tr>
+                        <th scope="row">mg.tasdiq.sakar@Gmail.com</th>
+                    </tr>
+                <tbody className="">
+                    <tr className="">
+                        <td className="md:p-2"> Name </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Muhammad Ash Shidiqi</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Phone </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> +62 89531032995</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Age </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> 26</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Address </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Sukabumi, West Java Indonesia</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> last Education </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Madrasah Aliyah Negeri 2</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Marital status </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Dating</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Nationality </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> NKRI(HARGA MATI)</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Activity </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Studying </td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Skills </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Mengikuti Tutorial di You Tube</td>
+                    </tr>
+                    <tr>
+                        <td className="md:p-2"> Objektif </td>
+                        <td className="md:p-2"> : </td>
+                        <td className="md:p-2"> Saya sangat senang dalam mencari pengalaman baru terutama dalam bidang IT yang bersangkut paut dengan pemrograman</td>
+                    </tr>
+                </tbody>
+            </table>
+            <?= $this->endSection(); ?>
+        </div>
+    </div>
+</div>
